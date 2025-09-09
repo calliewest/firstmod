ba124a887b0cb87e62a6c7727ab6d88987cda54d
maddie <battsparkour123123@gmail.com>
2025-09-08 20:33:35 -0400

    first

---
+++ src/main/java/net/cal/firstmod/firstmodClient.java
@@ -0,0 +1,9 @@
+package net.cal.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+public class firstmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
