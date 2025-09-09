56c93da389462e71dbee4b8979932180d18ab470
maddie <battsparkour123123@gmail.com>
2025-09-08 22:20:59 -0400

    tooth added

--- src/main/java/net/cal/firstmod/FirstMod.java
+++ src/main/java/net/cal/firstmod/FirstMod.java
@@ -1,5 +1,6 @@
 package net.cal.firstmod;
 
+import net.cal.firstmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,7 +19,7 @@ public class FirstMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/cal/firstmod/item/ModItems.java
+++ src/main/java/net/cal/firstmod/item/ModItems.java
@@ -1,2 +1,28 @@
-package net.cal.firstmod.item;public class ModItems {
+package net.cal.firstmod.item;
+
+import net.cal.firstmod.FirstMod;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item TOOTH = registerItem("tooth",new Item(new Item.Settings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(TOOTH);
+    }
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(FirstMod.MOD_ID, name), item);
+    }
+    public static void registerModItems(){
+        FirstMod.LOGGER.info("Registering mod items for " + FirstMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
 }
